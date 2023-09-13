181a471d25e4bb090bbb19fcd5f2c828a8dc565a
IT21239298 <it21239298@my.sliit.lk>
2023-09-13 12:08:07 +0530

    Add router-dom and change path

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import logo from "../assest/logo.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="fixed shadow-md w-full h-16">
+    <header className="fixed shadow-md w-full h-16 px-2 md:px-4">
       {/* desktop */}
 
-      <div className="">
-        <div className="h-12">
-          <img src={logo} className="h-full" />
-        </div>
+      <div className="flex items-center h-full">
+        <Link to={""}>
+          <div className="h-12">
+            <img src={logo} className="h-full" />
+          </div>
+        </Link>
       </div>
 
       {/* mobile */}
