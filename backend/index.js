db289bffb0dd29dd30e1a2c32cface8512d1eb19
IT21239298 <it21239298@my.sliit.lk>
2023-09-14 00:26:05 +0530

    login api create

--- backend/index.js
+++ backend/index.js
@@ -54,5 +54,33 @@ app.post("/signup", async (req, res) => {
   });
 });
 
+//api login
+app.post("/login", (req, res) => {
+  // console.log(req.body);
+  const { email } = req.body;
+  userModel.findOne({ email: email }, (err, result) => {
+    if (result) {
+      const dataSend = {
+        _id: result._id,
+        firstName: result.firstName,
+        lastName: result.lastName,
+        email: result.email,
+        image: result.image,
+      };
+      console.log(dataSend);
+      res.send({
+        message: "Login is successfully",
+        alert: true,
+        data: dataSend,
+      });
+    } else {
+      res.send({
+        message: "Email is not available, please sign up",
+        alert: false,
+      });
+    }
+  });
+});
+
 //server is ruuning
 app.listen(PORT, () => console.log("server is running at port : " + PORT));
