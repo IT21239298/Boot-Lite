a5140a4ff4c52f8cb75e1efcff4f28b2ad9f3257
IT21239298 <it21239298@my.sliit.lk>
2023-09-14 00:18:10 +0530

    done sign up backend

--- backend/index.js
+++ backend/index.js
@@ -16,13 +16,43 @@ mongoose
   .then(() => console.log("Connect to Databse"))
   .catch((err) => console.log(err));
 
+//schema
+const userSchema = mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: String,
+  confirmPassword: String,
+  image: String,
+});
+
+//model
+const userModel = mongoose.model("user", userSchema);
 //api
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
+//sign up
 app.post("/signup", async (req, res) => {
   console.log(req.body);
+  const { email } = req.body;
+
+  userModel.findOne({ email: email }, (err, result) => {
+    console.log(result);
+    console.log(err);
+    if (result) {
+      res.send({ message: "Email id is already register", alert: false });
+    } else {
+      const data = userModel(req.body);
+      const save = data.save();
+      res.send({ message: "Successfully Sign Up...!", alert: true });
+    }
+  });
 });
+
 //server is ruuning
 app.listen(PORT, () => console.log("server is running at port : " + PORT));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Header from "./component/Header";
+import { Toaster, toast } from "react-hot-toast";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <main className="pt-16 bg-slate-100 min-h-[calc(100vh)]">
-        <Outlet />
-      </main>
-    </div>
+    <>
+      <Toaster />
+      <div>
+        <Header />
+        <main className="pt-16 bg-slate-100 min-h-[calc(100vh)]">
+          <Outlet />
+        </main>
+      </div>
+    </>
   );
 }
 export default App;
