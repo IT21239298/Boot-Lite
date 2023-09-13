19f78b4a4837109095508f3f25ac2915c57fa319
IT21239298 <it21239298@my.sliit.lk>
2023-09-13 13:42:49 +0530

    signup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,6 +13,7 @@ import Home from "./page/Home";
 import Menu from "./page/Menu";
 import Login from "./page/login";
 import NewProduct from "./page/NewProduct";
+import Signup from "./page/Signup";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -21,6 +22,7 @@ const router = createBrowserRouter(
       <Route path="menu" element={<Menu />} />
       <Route path="login" element={<Login />} />
       <Route path="newproduct" element={<NewProduct />} />
+      <Route path="signup" element={<Signup />} />
     </Route>
   )
 );
@@ -28,7 +30,4 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={router} />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
