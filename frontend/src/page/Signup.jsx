68c0f3376f45a4317ea2f154e34440d308a72883
IT21239298 <it21239298@my.sliit.lk>
2023-09-13 16:13:02 +0530

    don sign up

--- frontend/src/page/Signup.jsx
+++ frontend/src/page/Signup.jsx
@@ -6,6 +6,16 @@ import { Link } from "react-router-dom";
 const Signup = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    image: "",
+  });
+
+  console.log(data);
 
   const handleShowPassword = () => {
     setShowPassword((preve) => !preve);
@@ -13,6 +23,32 @@ const Signup = () => {
 
   const handleShowConfirmPassword = () => {
     setShowConfirmPassword((preve) => !preve);
+  };
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setData((preve) => {
+      return {
+        ...preve,
+        [name]: value,
+      };
+    });
+  };
+  {
+    /*check paword and confirm password equal and validation */
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { firstName, email, password, confirmPassword } = data;
+    if (firstName && email && password && confirmPassword) {
+      if (password === confirmPassword) {
+        alert("Successfull");
+      } else {
+        alert("password and confirm password not equal");
+      }
+    } else {
+      alert("Please Enter required fields");
+    }
   };
 
   return (
@@ -23,13 +59,15 @@ const Signup = () => {
           <div className="w-20 h-20 overflow-hidden rounded-full drop-shadow-md shadow-md m-auto relative ">
             <img src={loginSignupImage} className="w-full" />
           </div>
-          <form className="w-full py-3 flex flex-col">
+          <form className="w-full py-3 flex flex-col" onSubmit={handleSubmit}>
             <label htmlFor="firstName">First Name</label>
             <input
               type={"text"}
-              id="lastName"
-              name="lastName"
+              id="firstName"
+              name="firstName"
               className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
+              value={data.firstName}
+              onChange={handleOnChange}
             />
             <label htmlFor="lastName">Last Name</label>
             <input
@@ -37,6 +75,8 @@ const Signup = () => {
               id="lastName"
               name="lastName"
               className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
+              value={data.lastName}
+              onChange={handleOnChange}
             />
 
             <label htmlFor="email">Email</label>
@@ -45,6 +85,8 @@ const Signup = () => {
               id="email"
               name="email"
               className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
+              value={data.email}
+              onChange={handleOnChange}
             />
             <label htmlFor="password">Password</label>
             <div className="flex px-2 py-1 bg-slate-200 rounded mt-1 mb-2 focus-within:outline focus-within:outline-blue-300">
@@ -53,6 +95,8 @@ const Signup = () => {
                 id="password"
                 name="password"
                 className=" w-full bg-slate-200 border-none outline-none "
+                value={data.password}
+                onChange={handleOnChange}
               />
               {/*handle password show*/}
               <span
@@ -67,9 +111,11 @@ const Signup = () => {
             <div className="flex px-2 py-1 bg-slate-200 rounded mt-1 mb-2  focus-within:outline focus-within:outline-blue-300">
               <input
                 type={showConfirmPassword ? "text" : "password"}
-                id="confirmpassword"
+                id="confirmPassword"
                 name="confirmPassword"
                 className=" w-full bg-slate-200 border-none outline-none "
+                value={data.confirmPassword}
+                onChange={handleOnChange}
               />
               <span
                 className="flex text-xl cursor-pointer"
@@ -79,7 +125,10 @@ const Signup = () => {
               </span>
             </div>
 
-            <button className="w-full max-w-[150px] m-auto  bg-blue-600 hover:bg-blue-700 cursor-pointer  text-white text-xl font-medium text-center py-1 rounded-full mt-4">
+            <button
+              type="submit"
+              className="w-full max-w-[150px] m-auto  bg-blue-600 hover:bg-blue-700 cursor-pointer  text-white text-xl font-medium text-center py-1 rounded-full mt-4"
+            >
               Sign up
             </button>
             <p className="text-left text-sm mt-2">
