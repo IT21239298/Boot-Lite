f1d8b5b7040404af28c22b8ea9904713310cbe4e
IT21239298 <it21239298@my.sliit.lk>
2023-09-14 19:49:21 +0530

    add quntity

--- frontend/src/page/Cart.jsx
+++ frontend/src/page/Cart.jsx
@@ -1,18 +1,57 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import CartProduct from "../component/cartProduct";
 
 const Cart = () => {
   const productCartItem = useSelector((state) => state.product.cartItem);
   console.log(productCartItem);
+
+  const totalPrice = productCartItem.reduce(
+    (acc, curr) => acc + parseInt(curr.total),
+    0
+  );
+  const totalQty = productCartItem.reduce(
+    (acc, curr) => acc + parseInt(curr.qty),
+    0
+  );
 
   return (
     <div className="p-2 md:p-4">
       <h2 className="text-lg md:text-2xl font-bold text-slate-600">
         Your Cart Items
       </h2>
-      <div className="">
+      <div className="my-4 flex gap-3">
         {/* display cart items*/}
+        <div className="w-full max-w-3xl">
+          {productCartItem.map((el) => {
+            return (
+              <CartProduct
+                key={el._id}
+                id={el._id}
+                image={el.image}
+                model={el.model}
+                brand={el.brand}
+                price={el.price}
+                qty={el.qty}
+                total={el.total}
+              />
+            );
+          })}
+        </div>
         <div className="">{/* total cart item*/}</div>
+        <div className="w-full max-w-md  ml-auto">
+          <h2 className="bg-blue-500 text-white p-2 text-lg">Summary</h2>
+          <div className="flex w-full py-2 text-lg border-b">
+            <p>Total Qty :</p>
+            <p className="ml-auto w-32 font-bold">{totalQty}</p>
+          </div>
+          <div className="flex w-full py-2 text-lg border-b">
+            <p>Total Price</p>
+            <p className="ml-auto w-32 font-bold">
+              <span className="text-red-500">₹</span> {totalPrice}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
--- frontend/src/redux/productSlice.js
+++ frontend/src/redux/productSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-hot-toast";
 
 const initialState = {
   productList: [],
@@ -14,14 +15,48 @@ export const productSlice = createSlice({
       state.productList = [...action.payload];
     },
     addCartItem: (state, action) => {
-      console.log(action);
-      const total = action.payload.price;
-      state.cartItem = [
-        ...state.cartItem,
-        { ...action.payload, qty: 1, total: total },
-      ];
+      const check = state.cartItem.some((el) => el._id === action.payload._id);
+      if (check) {
+        toast("Already Item in Cart");
+      } else {
+        toast("Item Add successfully");
+        const total = action.payload.price;
+        state.cartItem = [
+          ...state.cartItem,
+          { ...action.payload, qty: 1, total: total },
+        ];
+      }
     },
-    deleteCartItem: (state, action) => {},
+    deleteCartItem: (state, action) => {
+      toast("one Item Delete");
+      const index = state.cartItem.findIndex((el) => el._id === action.payload);
+      state.cartItem.splice(index, 1);
+      console.log(index);
+    },
+    increaseQty: (state, action) => {
+      const index = state.cartItem.findIndex((el) => el._id === action.payload);
+      let qty = state.cartItem[index].qty;
+      const qtyInc = ++qty;
+      state.cartItem[index].qty = qtyInc;
+
+      const price = state.cartItem[index].price;
+      const total = price * qtyInc;
+
+      state.cartItem[index].total = total;
+    },
+    decreaseQty: (state, action) => {
+      const index = state.cartItem.findIndex((el) => el._id === action.payload);
+      let qty = state.cartItem[index].qty;
+      if (qty > 1) {
+        const qtyDec = --qty;
+        state.cartItem[index].qty = qtyDec;
+
+        const price = state.cartItem[index].price;
+        const total = price * qtyDec;
+
+        state.cartItem[index].total = total;
+      }
+    },
   },
 });
 
